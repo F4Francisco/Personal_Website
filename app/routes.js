896c2fe703c9424987788fea1d2a146287461016
Francisco Perez <Francisco.Perez4@lc.cuny.edu>
2016-12-20 12:44:33 -0500

    First commit

---
+++ app/routes.js
@@ -0,0 +1,31 @@
+// require express
+var express = require('express');
+var path    = require('path');
+
+// create our router object
+var router = express.Router();
+
+// export our router
+module.exports = router;
+
+// route for our homepage
+router.get('/', function(req, res) {
+  res.render('pages/home');
+});
+
+// route for our about page
+router.get('/about', function(req, res) {
+  var users = [
+    { name: 'Francisco Perez', email: 'F4Francisco@gmail.com', avatar: 'https://s11.postimg.org/e7ejhh80j/pic.jpg'}
+  ];
+
+  res.render('pages/about', { users: users });
+});
+
+router.get('/contact', function(req, res) {
+  res.render('pages/contact');
+});
+
+router.post('/contact', function(req, res) {
+  res.send('Thanks for contacting us, ' + req.body.name + '! We will respond shortly!');
+});
